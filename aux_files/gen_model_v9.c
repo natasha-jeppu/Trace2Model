6205c7c59915e01baa91a38c6191d6fc2f8cb182
natppu <natppu@OUCL12554.local>
2020-05-26 09:35:26 +0100

    fixes to .yml and README

--- aux_files/gen_model_v9.c
+++ aux_files/gen_model_v9.c
@@ -1,25 +1,16 @@
-// ******************************************** Iteration:31
+// ******************************************** Iteration:0
 
 #include<stdio.h>
 #include<stdbool.h>
 #include<stdint.h>
 void main()
 {
-	uint8_t event_seq_length = 14;
-	uint8_t num_input = 1;
-	uint8_t event_seq[1][14] = {{1,2,4,3,7,5,8,7,9,10,6,5,8,7}};
-	uint8_t num_states = 9;
-	uint8_t t[48][3];
+	uint8_t event_seq_length = 3;
+	uint8_t num_input = 12;
+	uint8_t event_seq[12][3] = {{1,2,3},{2,3,4},{3,4,5},{4,5,4},{5,4,5},{5,4,6},{4,6,3},{6,3,4},{5,4,4},{4,4,7},{4,7,2},{7,2,3}};
+	uint8_t num_states = 5;
+	uint8_t t[36][3];
 	uint8_t count=0;
-	uint8_t t_gen[34][3] = {{1,1,4},{2,5,6},{2,5,7},{2,6,2},{2,6,7},{2,7,2},{2,7,7},{3,4,2},{3,4,5},{3,4,6},{4,2,3},{4,2,5},{5,3,2},{5,3,3},{5,3,6},{6,6,2},{6,6,6},{6,6,7},{6,7,2},{6,7,6},{6,7,7},{7,6,2},{7,6,7},{7,7,2},{7,7,7},{7,8,6},{7,8,8},{7,8,9},{8,10,6},{8,10,9},{9,6,7},{9,7,8},{9,9,6},{9,9,8}};
-
-	for(uint8_t i=0;i<34;i++)				
-		{																		
-			t[count][0] = t_gen[i][0];											
-			t[count][1] = t_gen[i][1];											
-			t[count][2] = t_gen[i][2];											
-			count = count + 1;													
-		}
  																		
 	for (uint8_t i=0;i<num_input;i++) 													
 	{																				
@@ -42,11 +33,11 @@ void main()
 		}																			
 	}
 
-	bool in[num_states][10];												
-	bool o[num_states][10];														
+	bool in[num_states][7];												
+	bool o[num_states][7];														
 																					
 	for (uint8_t i=0;i<num_states;i++)												
-		for (uint8_t j=0;j<10;j++)													
+		for (uint8_t j=0;j<7;j++)													
 		{																			
 			in[i][j] = false;														
 			o[i][j] = false;														
@@ -61,25 +52,19 @@ void main()
 	bool wrong_transition = false;														
 	for (uint8_t i=0; i<num_states;i++)														
 	{																				
-			if (in[i][ 0] && (o[i][0] || o[i][2] || o[i][3] || o[i][4] || o[i][5] || o[i][6] || o[i][7] || o[i][8] || o[i][9] || o[i][10]))
+			if (in[i][ 0] && (o[i][0] || o[i][2] || o[i][3] || o[i][4] || o[i][5] || o[i][6]))
 				wrong_transition = true;
-			else if (in[i][ 1] && (o[i][0] || o[i][1] || o[i][4] || o[i][5] || o[i][6] || o[i][7] || o[i][8] || o[i][9] || o[i][10]))
+			else if (in[i][ 1] && (o[i][0] || o[i][1] || o[i][3] || o[i][4] || o[i][5] || o[i][6]))
 				wrong_transition = true;
-			else if (in[i][ 2] && (o[i][0] || o[i][1] || o[i][2] || o[i][7] || o[i][8] || o[i][9] || o[i][10]))
+			else if (in[i][ 2] && (o[i][0] || o[i][1] || o[i][2] || o[i][4] || o[i][5] || o[i][6]))
 				wrong_transition = true;
-			else if (in[i][ 3] && (o[i][0] || o[i][1] || o[i][3] || o[i][7] || o[i][8] || o[i][9] || o[i][10]))
+			else if (in[i][ 3] && (o[i][0] || o[i][1] || o[i][2]))
 				wrong_transition = true;
-			else if (in[i][ 4] && (o[i][0] || o[i][1] || o[i][2] || o[i][3] || o[i][4] || o[i][8] || o[i][9] || o[i][10]))
+			else if (in[i][ 4] && (o[i][0] || o[i][1] || o[i][2] || o[i][4] || o[i][5] || o[i][6]))
 				wrong_transition = true;
-			else if (in[i][ 5] && (o[i][0] || o[i][1] || o[i][2] || o[i][3] || o[i][8] || o[i][9] || o[i][10]))
+			else if (in[i][ 5] && (o[i][0] || o[i][1] || o[i][3] || o[i][4] || o[i][5] || o[i][6]))
 				wrong_transition = true;
-			else if (in[i][ 6] && (o[i][1] || o[i][2] || o[i][3] || o[i][10]))
-				wrong_transition = true;
-			else if (in[i][ 7] && (o[i][1] || o[i][2] || o[i][3] || o[i][4] || o[i][7] || o[i][10]))
-				wrong_transition = true;
-			else if (in[i][ 8] && (o[i][0] || o[i][1] || o[i][2] || o[i][3] || o[i][4] || o[i][7] || o[i][8] || o[i][10]))
-				wrong_transition = true;
-			else if (in[i][ 9] && (o[i][0] || o[i][1] || o[i][2] || o[i][3] || o[i][4] || o[i][7] || o[i][9] || o[i][10]))
+			else if (in[i][ 6] && (o[i][0] || o[i][2] || o[i][3] || o[i][4] || o[i][5] || o[i][6]))
 				wrong_transition = true;
 	}
 	assert(wrong_transition != false);
